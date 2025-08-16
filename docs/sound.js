147a5a95668c4c4d74ef772832464fd5d296bd2c
Matheus M. <matheusmoraesdj2025@gmail.com>
2025-08-15 23:44:59 -0300

    Delete docs directory

--- docs/sound.js
+++
@@ -1,9 +0,0 @@
-// Sons já carregados via index.html
-// Coin, Boost, Event, Prestígio
-// Podem ser adicionados mais futuramente para upgrades, conquistas e eventos especiais
-
-// Exemplo de som extra para upgrade
-function playUpgradeSound(){
-    const s = new Audio('https://www.soundjay.com/button/sounds/button-5.mp3');
-    s.play();
-}
